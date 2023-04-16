4ef30c5559caf7cc6757e14e23ddd9683139dc31
Daniel Ohrin <xqiam@outlook.com>
2023-04-16 14:05:10 -0500

    Initial Commit

---
+++ SQL/TerrariaDB/01_Create_Script.sql
@@ -0,0 +1,221 @@
+use [master]
+GO
+
+DROP DATABASE IF EXISTS Terraria
+GO
+
+CREATE DATABASE Terraria
+GO
+
+use [Terraria]
+GO
+
+CREATE TABLE Rarity (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(25),
+
+	CONSTRAINT [UQ_Rarity_Label] UNIQUE([Label])
+)
+
+CREATE TABLE GameStage (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+	IsHardmode bit,
+
+	CONSTRAINT [UQ_GameStage_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Item (
+	Id int PRIMARY KEY IDENTITY,
+	[Name] nvarchar(100),
+	InternalName nvarchar(100),
+	Tooltip nvarchar(300),
+	MaxStackAmount int,
+	SellPrice int,
+	ResearchPoints int,
+	ImageUrl nvarchar(200),
+	RarityId int,
+	GameStageId int,
+
+	CONSTRAINT [UQ_Item_Name] UNIQUE([Name]),
+	CONSTRAINT [UQ_Item_InternalName] UNIQUE([InternalName]),
+	CONSTRAINT [FK_Item_RarityId] FOREIGN KEY (RarityId) REFERENCES Rarity(Id),
+	CONSTRAINT [FK_Item_GameStageId] FOREIGN KEY (GameStageId) REFERENCES GameStage(Id)
+)
+
+CREATE TABLE [Type] (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+	
+	CONSTRAINT [UQ_Type_Label] UNIQUE([Label])
+)
+
+CREATE TABLE ItemType (
+	Id int PRIMARY KEY IDENTITY,
+	ItemId int,
+	TypeId int,
+
+	CONSTRAINT [FK_ItemType_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id),
+	CONSTRAINT [FK_ItemType_TypeId] FOREIGN KEY (TypeId) REFERENCES [Type](Id)
+)
+
+CREATE TABLE ToolType (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+
+	CONSTRAINT [UQ_ToolType_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Tool (
+	Id int PRIMARY KEY IDENTITY,
+	HammerPower int,
+	AxePower int,
+	PickaxePower int,
+	Damage int,
+	Bonus nvarchar(25),
+	UseTime int,
+	MiningSpeed int,
+	Knockback dec(18, 2),
+	ItemId int,
+	ToolTypeId int,
+
+	CONSTRAINT [FK_Tool_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id),
+	CONSTRAINT [FK_Tool_ToolTypeId] FOREIGN KEY (ToolTypeId) REFERENCES ToolType(Id)
+)
+
+CREATE TABLE WeaponType (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+
+	CONSTRAINT [UQ_WeaponType_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Weapon (
+	Id int PRIMARY KEY IDENTITY,
+	Damage int,
+	Knockback dec(18, 2),
+	CriticalChance int,
+	UseTime int,
+	Velocity int,
+	IsAutoSwing bit,
+	WeaponTypeId int,
+	ItemId int,
+
+	CONSTRAINT [FK_Weapon_WeaponTypeId] FOREIGN KEY (WeaponTypeId) REFERENCES WeaponType(Id),
+	CONSTRAINT [FK_Weapon_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE AmmunitionType (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+
+	CONSTRAINT [UQ_AmmunitionType_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Ammunition (
+	Id int PRIMARY KEY IDENTITY,
+	Damage int,
+	Velocity dec(18, 2),
+	VelocityMultiplier int,
+	Knockback dec(18, 2),
+	AmmunitionTypeId int,
+	ItemId int,
+
+	CONSTRAINT [FK_Ammunition_AmmunitionTypeId] FOREIGN KEY (AmmunitionTypeId) REFERENCES AmmunitionType(Id),
+	CONSTRAINT [FK_Ammunition_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE ArmorSet (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+	Bonus nvarchar(200),
+
+	CONSTRAINT [UQ_ArmorSet_Label] UNIQUE([Label])
+)
+
+CREATE TABLE ArmorSlot (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(20),
+
+	CONSTRAINT [UQ_ArmorSlot_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Armor (
+	Id int PRIMARY KEY IDENTITY,
+	Defense int,
+	ArmorSetId int,
+	ArmorSlotId int,
+	ItemId int,
+
+	CONSTRAINT [FK_Armor_ArmorSetId] FOREIGN KEY (ArmorSetId) REFERENCES ArmorSet(Id),
+	CONSTRAINT [FK_Armor_ArmorSlotId] FOREIGN KEY (ArmorSlotId) REFERENCES ArmorSlot(Id),
+	CONSTRAINT [FK_Armor_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE Bait (
+	Id int PRIMARY KEY IDENTITY,
+	[Power] int,
+	UseTime int,
+	IsConsumable bit,
+	ItemId int,
+
+	CONSTRAINT [FK_Bait_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE Bar (
+	Id int PRIMARY KEY IDENTITY,
+	UseTime int,
+	IsPlaceable bit,
+	ItemId int,
+
+	CONSTRAINT [FK_Bar_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE FurnitureSet (
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+
+	CONSTRAINT [UQ_FurnitureSet_Label] UNIQUE([Label])
+)
+
+CREATE TABLE Furniture (
+	Id int PRIMARY KEY IDENTITY,
+	UseTime int,
+	IsPlaceable bit,
+	IsCraftingStation bit,
+	FurnitureSetId int,
+	ItemId int,
+
+	CONSTRAINT [FK_Furniture_FurnitureSetId] FOREIGN KEY (FurnitureSetId) REFERENCES FurnitureSet(Id),
+	CONSTRAINT [FK_Furniture_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+--TODO: Make a table to store the possible drops for GrabBags
+CREATE TABLE GrabBag ( --Like a lootbox or something you can open for rewards
+	Id int PRIMARY KEY IDENTITY,
+	[Label] nvarchar(50),
+	UseTime int,
+	IsPlaceable bit,
+	ItemId int,
+
+	CONSTRAINT [UQ_GrabBag_Label] UNIQUE([Label]),
+	CONSTRAINT [FK_GrabBag_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
+
+CREATE TABLE Buff (
+	Id int PRIMARY KEY IDENTITY,
+	Tooltip nvarchar(100),
+	Duration int, --Seconds the buff lasts
+	ImageUrl nvarchar(200),
+	IsDebuff bit
+)
+
+CREATE TABLE Potion (
+	Id int PRIMARY KEY IDENTITY,
+	UseTime int,
+	BuffId int,
+	ItemId int,
+
+	CONSTRAINT [FK_Potion_BuffId] FOREIGN KEY (BuffId) REFERENCES Buff(Id),
+	CONSTRAINT [FK_Potion_ItemId] FOREIGN KEY (ItemId) REFERENCES Item(Id)
+)
