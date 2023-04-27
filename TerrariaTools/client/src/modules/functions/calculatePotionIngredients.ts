34bd55221b74f86204d290732d299cd8154947af
Daniel Ohrin <xqiam@outlook.com>
2023-04-27 14:43:18 -0500

    Resolved conversations && moved error handling for calculatePotionIngredients

--- TerrariaTools/client/src/modules/functions/calculatePotionIngredients.ts
+++ TerrariaTools/client/src/modules/functions/calculatePotionIngredients.ts
@@ -13,65 +13,63 @@ export interface CombinedIngredient {
  * @returns A list of potionIngredients, amount, and group 
  */
 export function calculatePotionIngredients(potions: Potion[]): CombinedIngredient[] {
+
     let group = 1
 
     const result: CombinedIngredient[] = []
 
-    potions.forEach((potion, potionIndex) => {
+    potions.forEach(potion => {
         //? Store the amount of crafts in a variable
         const amount = potion.getAmount()
 
         //? If that variable is <1, no calculations are needed
         if (amount < 1) return;
 
-        //* Store the list of recipes for the potion in a variable 
-        const recipes = potion.getRecipes()
+        //! Check if the potion has a valid recipe
+        if (potion.getRecipes().some(recipe => recipe.potionIngredients.some(ingredient => !ingredient.potion && !ingredient.ingredient))) {
+        throw Error("The provided list of Potion[] contains potion(s) without recipe ingredients.")
+    }
 
-        //* Store the list of ingredients in an array 
-        const ingredients: PotionIngredient[] = []
+    //* Store the list of recipes for the potion in a variable 
+    const recipes = potion.getRecipes()
 
-        for (const recipe of recipes) {
-            recipe.potionIngredients.forEach(ing => {
-                if (!ingredients.some(addedIng => (addedIng.ingredientId ?? addedIng.potionId) === (ing.ingredientId ?? ing.potionId))) {
-                    ingredients.push(ing)
-                }
-            })
-        }
+    //* Store the list of ingredients in an array 
+    const ingredients: PotionIngredient[] = []
 
-        //! Loop over each ingredient to store it in the "result" array
-        ingredients.forEach((ing, index) => {
-            //? Create a variable to check if the ingredient is already among the list of ingredients
-            let existingIngredient: CombinedIngredient | undefined
-            if (!ing.potion && !ing.ingredient) throw Error(`This PotionIngredient has no Material or Potion: Potion#${potionIndex}`);
-
-            //? Assign a value to existingIngredient
-            if (ing.potion) {
-                existingIngredient = result.find(obj => obj.material.name === ing.potion?.name)
-            } else {
-                existingIngredient = result.find(obj => obj.material.name === ing.ingredient?.name)
-            }
-
-            //! Check if the ingredient is present in every recipe for the potion
-            const isShared: Boolean = recipes.every(recipe => recipe.potionIngredients.some(x => (x.ingredientId ?? x.potionId) === (ing.ingredientId ?? ing.potionId)))
-            //* Add the ingredient to the "result" array or update its amount
-            if (typeof existingIngredient === "undefined") {
-                const newMaterial: CombinedIngredient = {
-                    material: (ing.potion ?? ing.ingredient)!,
-                    amount: ing.amount
-                }
-
-                if (!isShared) {
-                    newMaterial.group = group
-                }
-
-                result.push(newMaterial)
-            } else {
-                existingIngredient.amount += ing.amount
+    for (const recipe of recipes) {
+        recipe.potionIngredients.forEach(ing => {
+            if (!ingredients.some(addedIng => (addedIng.ingredientId ?? addedIng.potionId) === (ing.ingredientId ?? ing.potionId))) {
+                ingredients.push(ing)
             }
         })
+    }
 
-        //! Increase the group # as needed
-        if (recipes.length > 1) group++;
+    //! Loop over each ingredient to store it in the "result" array
+    ingredients.forEach(ing => {
+        //? Create a variable to check if the ingredient is already among the list of ingredients
+        let existingIngredient: CombinedIngredient | undefined = result.find(obj => obj.material.name === (ing.ingredient?.name ?? ing.potion?.name))
+
+        //! Check if the ingredient is present in every recipe for the potion
+        const isShared: Boolean = recipes.every(recipe => recipe.potionIngredients.some(x => (x.ingredientId ?? x.potionId) === (ing.ingredientId ?? ing.potionId)))
+        //* Add the ingredient to the "result" array or update its amount
+        if (typeof existingIngredient === "undefined") {
+            const newMaterial: CombinedIngredient = {
+                material: (ing.potion ?? ing.ingredient)!,
+                amount: ing.amount
+            }
+
+            if (!isShared) {
+                newMaterial.group = group
+            }
+
+            result.push(newMaterial)
+        } else {
+            existingIngredient.amount += ing.amount
+        }
     })
-    return result
+
+    //! Increase the group # as needed
+    if (recipes.length > 1) group++;
+})
+return result
 }
